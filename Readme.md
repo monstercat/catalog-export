820d24249df63cc11ea8a5640b55d8cb19d90c3f
William Casarin <bill@casarin.me>
2014-08-01 12:26:27 -0700

    add contract to catalog export

--- Readme.md
+++ Readme.md
@@ -3,6 +3,10 @@
 
   Monstercat catalog export
 
+## Instructions
+
+    $ ./index.js > catalog.json
+
 ## License
 
   Copyright (c) 2014 Monstercat
--- index.js
+++ index.js
@@ -9,28 +9,41 @@ let co   = require('co');
 let db = monk('localhost/test');
 let Album = wrap(db.get('Album'));
 let Track = wrap(db.get('Track'));
+let Contract = wrap(db.get('Contract'));
 
 module.exports = function(done) {
   co(function*(){
     let albums = yield Album.find({})
     let tracks = yield Track.find({})
+    let contracts = yield Contract.find({})
+
+    let contractMap = contracts.reduce(function(map, contract){
+      map[contract.track] = contract;
+      return map;
+    }, {})
+
     let albumMap = albums.reduce(function(map, album){
       map[album._id] = album;
       return map;
     }, {})
+
     let items = tracks.reduce(function(arr, track){
+      let contract = contractMap[track._id];
       (track.albums || []).forEach(function(album){
         album = albumMap[album.albumId]
         arr.push({
+          contract: contract,
           release: album,
-          track: track,
+          track: track
         });
       });
       return arr;
     }, []);
+
     items.forEach(function(item){
       process.stdout.write(JSON.stringify(item) + "\n");
     });
+
     db.close();
   })(done);
 }
